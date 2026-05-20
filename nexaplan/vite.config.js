e2a1dc9344b81b5a09ed8499398894932401d3aa
Aliyu Aminu <aliyu.aminu@pwc.com>
2026-05-20 11:17:13 +0100

    Fix build

--- nexaplan/vite.config.js
+++ nexaplan/vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nexaplan/',
+  base: '/NexaPlan/',
   plugins: [
     vue(),
     vueDevTools(),
